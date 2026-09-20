1f335829b9ebc285f2735250e556eff1b2880b55
Aman Naveen <aman.naveen@ramanujan.du.ac.in>
2026-09-20 09:34:14 +0530

    Add DriveCam authentication and recording service

---
+++ app/src/main/java/com/aman/camera2drive/DriveAuth.kt
@@ -0,0 +1,25 @@
+package com.aman.camera2drive
+
+import android.app.Activity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.Scope
+
+object DriveAuth {
+    const val RC_SIGN_IN = 2401
+
+    fun signIn(activity: Activity, ready: () -> Unit) {
+        if (GoogleSignIn.getLastSignedInAccount(activity) != null) {
+            ready()
+            return
+        }
+        val options = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .requestScopes(Scope("https://www.googleapis.com/auth/drive.file"))
+            .build()
+        activity.startActivityForResult(
+            GoogleSignIn.getClient(activity, options).signInIntent,
+            RC_SIGN_IN
+        )
+    }
+}
