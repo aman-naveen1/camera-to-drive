0b72607a87af3123f7347b3a911fccf425ca9024
Aman Naveen <aman.naveen@ramanujan.du.ac.in>
2026-09-20 14:06:48 +0530

    Resolve duplicate Java resource packaging

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,6 +23,12 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/INDEX.LIST"
+        }
+    }
 }
 
 dependencies {
