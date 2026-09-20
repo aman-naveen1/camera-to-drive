f82add110449f0f6fa46891a95eea5af870651cb
Aman Naveen <aman.naveen@ramanujan.du.ac.in>
2026-09-20 14:57:19 +0530

    Add explicit Activity Result dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.activity:activity:1.13.0")
     implementation("androidx.activity:activity-ktx:1.13.0")
     implementation("androidx.camera:camera-camera2:1.6.2")
     implementation("androidx.camera:camera-lifecycle:1.6.2")
